0c2fe002b8aef8e10b9dbe9c8de24ec81a76fdae
Stefan Schade <dr_stefan_schade@yahoo.com>
2024-06-06 07:04:18 +0000

    git setup with env variables

--- scripts/post_create_script.sh
+++ scripts/post_create_script.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# some output for trouble shooting
+# some output for troubleshooting
 ls -la /workspace/scripts
 
+# Retrieve the Git user and email from command-line arguments
+GIT_USER="$1"
+GIT_EMAIL="$2"
+
 # configure git
-git config --global user.name 'Stefan Schade'
-git config --global user.email 'dr_stefan_schade@yahoo.com'
+git config --global user.name "$GIT_USER"
+git config --global user.email "$GIT_EMAIL"
+
+git config --list
 
 # make sure scripts are executable
 chmod +x /workspace/scripts/*.sh
