006e0cfb44231f30242c70dea2ce1bbf79fa3784
Stefan Schade <dr_stefan_schade@yahoo.com>
2024-06-07 18:05:00 +0000

    improve zscaler setup

--- scripts/post_create_script.sh
+++ scripts/post_create_script.sh
@@ -1,35 +1,43 @@
 #!/bin/bash
 
-# some output for troubleshooting
+# Some output for troubleshooting
 ls -la /workspace/scripts
 
 # Retrieve the Git user and email from command-line arguments
 GIT_USER="$1"
 GIT_EMAIL="$2"
 
-# configure git
+# Configure Git with the provided user name and email
 git config --global user.name "$GIT_USER"
 git config --global user.email "$GIT_EMAIL"
 
+# Display the current Git configuration
 git config --list
 
-# make sure scripts are executablev
+# Make sure scripts are executable
 chmod +x /workspace/scripts/*.sh
 
-# execute scripts
+# Execute additional setup scripts
 source /workspace/scripts/setup_ssh_git.sh 
 source /workspace/scripts/setup_docker_proxy.sh 
 source /workspace/scripts/install_python_dependencies.sh
 
-# Check for ZScaler certificate and add to CA store if it exists
+# Check for ZScaler certificate and add to default CA store if it exists
 CERT_PATH="/workspace/zscaler.crt"
+
 if [ -f "$CERT_PATH" ]; then
-    echo "ZScaler certificate found. Adding to CA store."
-    mkdir -p /usr/local/share/custom-ca-certificates
-    cp $CERT_PATH /usr/local/share/custom-ca-certificates/zscaler.crt
-    c_rehash /usr/local/share/custom-ca-certificates
-    echo "CApath = /usr/local/share/custom-ca-certificates" >> /etc/ssl/openssl.cnf
-    echo "ZScaler certificate added to CA store."
+    echo "ZScaler certificate found. Adding to default CA store."
+
+    # Copy the ZScaler certificate to the system CA directory
+    cp $CERT_PATH /usr/local/share/ca-certificates/zscaler.crt
+
+    # Ensure the certificate has correct permissions
+    chmod 644 /usr/local/share/ca-certificates/zscaler.crt
+
+    # Update the CA certificates
+    update-ca-certificates --fresh
+
+    echo "ZScaler certificate added to default CA store."
 else
     echo "ZScaler certificate not found. Skipping CA store update."
 fi
