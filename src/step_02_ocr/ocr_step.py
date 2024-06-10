5cea4043d5fe2c79e4e6a8f2a6be708ac8f71e27
Stefan Schade <dr_stefan_schade@yahoo.com>
2024-06-10 15:02:54 +0000

    move to json output

--- src/step_02_ocr/ocr_step.py
+++ src/step_02_ocr/ocr_step.py
@@ -3,6 +3,8 @@ from PIL import Image
 from pipeline_step import PipelineStep
 from step_02_ocr.utils_optimization import check_orientations
 from step_02_ocr.utils_tesseract import tesseract_ocr
+import json
+import logging
 
 class OCRStep(PipelineStep):
     def __init__(self, args):
@@ -15,14 +17,26 @@ class OCRStep(PipelineStep):
     def run(self, input_data):
         tessdata_dir_config = f'--tessdata-dir "{self.tessdata_dir}"'
         image_files = [f for f in os.listdir(input_data) if f.endswith(('.jpeg', '.jpg', '.png'))]
-        output_file = os.path.join(input_data, 'ocr_result.txt')
+        output_file = os.path.join(input_data, 'ocr_result.json')
 
         with open(output_file, 'w', encoding='utf-8') as file_out:
-            for image_file in image_files:
+            for index, image_file in enumerate(image_files, start=1):
                 img_path = os.path.join(input_data, image_file)
+                logging.info(f"Starting analysis of file: {image_file}")
                 img = Image.open(img_path)
                 text, final_angle, confidence = check_orientations(img, self.language, tessdata_dir_config, self.psm, self.check_orientation)
-                file_out.write(text + '\n')
+                
+                json_output = {
+                    "page_number": index,
+                    "source_file": image_file,
+                    "final_angle": final_angle,
+                    "confidence": confidence,
+                    "text": text
+                }
+                json.dump(json_output, file_out)
+                file_out.write('\n')
+                logging.debug(f"Processed {image_file} with final angle: {final_angle}")
+
                 # Save processed image if required
                 if self.save_preprocessed:
                     img.save(os.path.join(input_data, f"processed_{image_file}"))
