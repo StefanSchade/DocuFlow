867b495d57007af79e0817b254c23415ef350cc0
Stefan Schade <dr_stefan_schade@yahoo.com>
2024-06-26 17:44:19 +0000

    folowing typecrafts neovim course

---
+++ scripts/nvim_update.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Check if a configuration name is provided
+if [ -z "$2" ]; then
+    echo "Usage: $0 <config-name>"
+    exit 1
+fi
+
+CONFIG_NAME=$2
+SOURCE_PATH="/workspace/.config/nvim-$CONFIG_NAME"
+DEST_PATH="/root/.config/nvim"
+NVIM_STATE="/root/.local/state/nvim/"
+
+# Check if the source directory exists
+if [ ! -d "$SOURCE_PATH" ]; then
+    echo "Error: Configuration '$CONFIG_NAME' does not exist at $SOURCE_PATH"
+    exit 1
+fi
+
+# Check if the destination directory exists and prompt the user for confirmation
+if [ -d "$DEST_PATH" ]; then
+    echo -e "\e[31mWarning: This will overwrite your existing Neovim configuration in $DEST_PATH. Are you sure? (y/n)\e[0m"
+    read -p ">" choice
+    case "$choice" in 
+      y|Y ) 
+        rm -rf $NVIM_STATE/*
+        rm -rf $DEST_PATH/*
+        cp -r $SOURCE_PATH/* $DEST_PATH
+        echo "Neovim configuration replaced in $DEST_PATH"
+        ;;
+      n|N ) 
+        echo "Operation cancelled."
+        exit 0
+        ;;
+      * ) 
+        echo "Invalid input. Operation cancelled."
+        exit 1
+        ;;
+    esac
+else
+    cp -r $SOURCE_PATH/* $DEST_PATH
+    echo "Neovim configuration copied to $DEST_PATH"
+fi
--- scripts/update_nvim_config copy.sh
+++ scripts/update_nvim_config copy.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 # Define the source and destination paths
-SOURCE_PATH="/workspace/.config/nvim_"
+SOURCE_PATH="/workspace/.config/nvim"
 DEST_PATH="/root/.config/nvim"
 
 # Check if the source directory exists
 if [ -d "$SOURCE_PATH" ]; then
-    # Ensure the destination directory exists
-    mkdir -p "$DEST_PATH"
-    
     # Copy the Neovim configuration to the destination
-    cp -r "$SOURCE_PATH"/* "$DEST_PATH"
+    cp -r $SOURCE_PATH/* $DEST_PATH
     echo "Neovim configuration copied to $DEST_PATH"
 else
     echo "Source directory $SOURCE_PATH does not exist. Please check the path."
@@ -18,14 +15,15 @@ fi
 
 # Install vim-plug if not already installed
 if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
-    echo "Installing vim-plug for Neovim..."
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    echo "Installing vim-plug..."
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-# Install Python LSP server
 pip3 install -U setuptools pip
 pip3 install 'python-lsp-server[all]'
+echo "vim-plug installed."
 
 # Provide a message to run :PlugInstall inside nvim
 echo "Please run :PlugInstall inside nvim to install plugins."
+
