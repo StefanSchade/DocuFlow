02e591d63e2257639c13a1a3aba65d9c6a424534
Stefan Schade <dr_stefan_schade@yahoo.com>
2024-06-27 17:08:04 +0000

    following typecrafts neovim course - episode 2 complete

--- .config/nvim-typecraft/init.lua
+++ .config/nvim-typecraft/init.lua
@@ -1,10 +1,3 @@
-vim.g.mapleader = " "
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=3")
-
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -13,46 +6,14 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-    {
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v3.x",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-        "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-      }
-  }
-
-}
-
 local opts = {}
 
-require("lazy").setup(plugins, opts)
+require("vim-options")
+require("lazy").setup("plugins", opts)
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
 
-local configs = require("nvim-treesitter.configs")
 
-configs.setup({
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "rust", "python"},
-    sync_install = false,
-    highlight = { enable = true },
-    indent = { enable = true },  
-  })
 
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
+
+
